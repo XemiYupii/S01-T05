2401a22b3dea865e890d0490f44ea0df315c57ae
XemiYupii <120043339+XemiYupii@users.noreply.github.com>
2023-02-08 11:21:41 +0100

    Update App.java

--- exercici5/App.java
+++ exercici5/App.java
@@ -13,25 +13,25 @@ public class App {
 		Empleat treballador = new Empleat ("Gemma", 36, 1975.69);
 		
 		serialitzarObjecte(treballador);
-		Empleat treballador2 = desserialitzarObjecte(treballador);
-		System.out.println(desserialitzarObjecte(treballador));
+		Empleat treballador2 = desserialitzarObjecte();
+		System.out.println(desserialitzarObjecte());
 		
 	}
 	public static void serialitzarObjecte (Empleat empleat) {
 		try (ObjectOutputStream serialitzant = new ObjectOutputStream (new FileOutputStream ("serialitzacio.ser"))){
 			serialitzant.writeObject(empleat);
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Error al serialitzar Empleat " +e.getMessage());
 		}
 	}
-	public static Empleat desserialitzarObjecte (Empleat empleat) {
+	public static Empleat desserialitzarObjecte () {
 		Empleat treballador2 = null;
 		try (ObjectInputStream desserialitzant = new ObjectInputStream( new FileInputStream( "serialitzacio.ser"))){
 			Object objecteObtingut = desserialitzant.readObject();
 			if (objecteObtingut instanceof Empleat) 
 				treballador2 = (Empleat)objecteObtingut;
 		} catch (IOException | ClassNotFoundException e) {
-			System.out.println(e.getMessage());
+			System.out.println("Error al desserialitzar Empleat " +e.getMessage());
 		}
 		return treballador2;
 	}
